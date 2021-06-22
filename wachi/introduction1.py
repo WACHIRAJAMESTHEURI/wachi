068f163ae66562c7ee5b8977e2c2bbdc8c32bbd8
Wachira <45314805+WACHIRAJAMESTHEURI@users.noreply.github.com>
2021-06-23 00:38:06 +0300

    Add files via upload

---
+++ wachi/introduction1.py
@@ -0,0 +1,14 @@
+a =10
+print(a);
+import turtle
+wachi=turtle
+w=20
+j=20
+def hexagon():
+    for _ in range(18):
+        wachi.forward(w)
+        wachi.left(j)
+
+for _ in range(18):
+    wachi.forward(w)
+    wachi.left(j)
